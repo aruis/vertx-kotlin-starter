f7161e7e35259e058fb3babc3e71bd45ca39e029
aruis <lovearuis@gmail.com>
2017-07-24 10:43:08 +0800

    add Jackson test

--- src/main/kotlin/com/aruistart/study/examples/TestJackson.kt
+++ src/main/kotlin/com/aruistart/study/examples/TestJackson.kt
@@ -1,19 +1,21 @@
 package com.aruistart.study.examples
 
-import com.aruistart.study.MainVerticle
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
-val json = """{"wx": {"i": 224, "no": 4, "page": 12, "isSPU": 0, "state": 1}, "app": {"i": 298, "no": 8, "page": 30, "isSPU": 1, "state": "1"}, "web": {"i": 232, "no": 52, "page": 4, "isSPU": -1, "state": 1}}"""
+val json = """{"wx": {"i": 224, "no": 4, "page": 12, "isSPU": 0, "state": 1},
+"app": {"i": 298, "no": 8, "page": 30, "isSPU": 1, "state": "1"},
+"web": {"i": 232, "no": 52, "page": 4, "isSPU": -1, "state": 1}}"""
 
 data class Rank(val i: Int, val no: Int, val page: Int, val isSPU: Boolean, val state: String)
 data class AllRank(val app: Rank, val wx: Rank, val web: Rank, val xx: Rank?)
 
 fun main(args: Array<String>) {
     val mapper = jacksonObjectMapper()
-    val state: MainVerticle.AllRank = mapper.readValue(json)
+    val state: AllRank = mapper.readValue(json)
 
     println(json)
+    println(state)
     println(state.app)
     println(state.app.state::class.java.name)
     println(state.app.isSPU)
