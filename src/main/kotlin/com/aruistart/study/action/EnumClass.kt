b8fd59c68b3fcde682f597cb11d9c96636affa2b
aruis <lovearuis@gmail.com>
2017-07-26 14:49:45 +0800

    add some examples

---
+++ src/main/kotlin/com/aruistart/study/action/EnumClass.kt
@@ -0,0 +1,16 @@
+package com.aruistart.study.action
+
+enum class Color {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
+}
+
+fun getWarmth(color: Color) = when (color) {
+    Color.RED, Color.ORANGE, Color.YELLOW -> "warm"
+    Color.GREEN -> "neutral"
+    Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
+}
+
+fun main(args: Array<String>) {
+    println(getWarmth(Color.ORANGE))
+}
+
---
+++ src/main/kotlin/com/aruistart/study/action/TryAsAnExpression1.kt
@@ -0,0 +1,23 @@
+package com.aruistart.study.action
+
+import java.io.BufferedReader
+import java.io.StringReader
+
+fun readNumber(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        null
+    }
+
+    println(number)
+}
+
+fun main(args: Array<String>) {
+    val reader = BufferedReader(StringReader("not a number"))
+    readNumber(reader)
+
+    val reader2 = BufferedReader(StringReader("2"))
+    readNumber(reader2)
+}
+
