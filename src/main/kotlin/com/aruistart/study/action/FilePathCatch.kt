0a5f98606d44617fe275bf3311efc8083e015914
aruis <lovearuis@gmail.com>
2017-07-30 07:56:27 +0800

    add some examples

---
+++ src/main/kotlin/com/aruistart/study/action/FilePathCatch.kt
@@ -0,0 +1,15 @@
+package com.aruistart.study.action
+
+fun parsePath(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if (matchResult != null) {
+        val (directory, filename, extension) = matchResult.destructured
+        println("Dir: $directory, name: $filename, ext: $extension")
+    }
+}
+
+fun main(args: Array<String>) {
+    parsePath("/Users/yole/kotlin-book/chapter.adoc")
+}
+
