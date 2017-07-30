d972c4a61360432bb38d9d1b86b615bf3961bc48
aruis <lovearuis@gmail.com>
2017-07-30 08:01:24 +0800

    add some examples

---
+++ src/main/kotlin/com/aruistart/study/action/StringTrimMargin.kt
@@ -0,0 +1,10 @@
+package com.aruistart.study.action
+
+val kotlinLogo = """| //
+                   .|//
+                   .|/ \"""
+
+fun main(args: Array<String>) {
+    println(kotlinLogo.trimMargin("."))
+}
+
