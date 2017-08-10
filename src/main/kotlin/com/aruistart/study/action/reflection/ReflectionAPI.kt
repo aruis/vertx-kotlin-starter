f075c75e0cb3715923cae6eee9bf4dc7587e78a4
aruis <lovearuis@gmail.com>
2017-08-10 10:06:41 +0800

    add some examples

---
+++ src/main/kotlin/com/aruistart/study/action/reflection/ReflectionAPI.kt
@@ -0,0 +1,19 @@
+package com.aruistart.study.action.reflection
+
+import kotlin.reflect.memberProperties
+
+class Person(val name: String, val age: Int)
+
+/**
+ * NOTE: This example uses reflection, which is not supported in the "Try Kotlin" online environment.
+ * If you'd like to run it, please download the source code from https://www.manning.com/books/kotlin-in-action
+ * and run it on your local machine.
+ */
+fun main(args: Array<String>) {
+    val person = Person("Alice", 29)
+    val kClass = person.javaClass.kotlin
+    println(kClass.simpleName)
+    println(kClass.qualifiedName)
+    kClass.memberProperties.forEach { println(it.name) }
+}
+
