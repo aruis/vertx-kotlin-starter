7e5bd7c614c78c28c860d354a04df839edac2453
aruis <lovearuis@gmail.com>
2017-08-16 14:47:27 +0800

    test async db t

---
+++ src/main/groovy/com/aruistart/study/database/DatabaseVerticle.groovy
@@ -0,0 +1,74 @@
+package com.aruistart.study.database
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.DeploymentOptions
+import io.vertx.core.Future
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.asyncsql.AsyncSQLClient
+import io.vertx.ext.asyncsql.PostgreSQLClient
+import io.vertx.ext.sql.SQLConnection
+
+class DatabaseVerticle extends AbstractVerticle {
+
+
+    AsyncSQLClient client
+
+    static void main(String[] args) {
+        Vertx.vertx().deployVerticle(DatabaseVerticle.newInstance(), new DeploymentOptions().setInstances(1))
+    }
+
+    @Override
+    void start(Future<Void> startFuture) throws Exception {
+
+        client = PostgreSQLClient.createShared(vertx, new JsonObject([
+                host       : "127.0.0.1",
+                port       : 5432,
+                maxPoolSize: 1,
+                username   : "aruis",
+                password   : "aruis",
+                database   : "aruis"
+        ]))
+
+
+        client.getConnection({ ar ->
+            if (ar.succeeded()) {
+                SQLConnection conn = ar.result()
+
+//                conn.setAutoCommit(false, {
+                    conn.query("select * from test limit 3", { res ->
+
+                        conn.updateWithParams("update test set v_name = ? where id = ? ", ["aaabb", 1], { res2 ->
+
+                            println(res2.succeeded())
+
+                            throw new Exception("xxx")
+                            conn.query("select * from test limit 3", { res3 ->
+
+                                println(res3.result().rows)
+                            })
+                        })
+
+                        println(res.result().rows)
+                    })
+//                })
+
+//                conn.query("select * from test limit 3", { res ->
+//                    conn.close()
+//
+//
+//                    println(res.result().rows)
+//                })
+
+
+            }
+
+        })
+
+
+
+        super.start(startFuture)
+    }
+
+
+}
